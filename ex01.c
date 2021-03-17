18e176eacf72f130cc5c5c53b4ee3abdf4657827
Ariimb <78215533+Ariimb@users.noreply.github.com>
2021-03-16 19:15:03 -0600

    Update ex01.c

--- ex01.c
+++ ex01.c
@@ -1,6 +1,6 @@
 1. Find the Max of three numbers.
 
-int main() {
+    int main() {
     double n1, n2, n3;
     printf("Enter three different numbers: ");
     scanf("%lf %lf %lf", &n1, &n2, &n3);
@@ -19,7 +19,7 @@ int main() {
 
 2. Write a function to sum all the numbers in a list.
  
-int main()
+    int main()
 {
     int a[1000],i,n,sum=0;
    
@@ -44,3 +44,125 @@ int main()
 }
 
 3. Write a function to multiply all the numbers in a list.
+    
+4. Write a program to reverse a string.
+
+    int main()
+{
+   char s[100];
+
+   printf("Enter a string to reverse\n");
+   gets(s);
+
+   strrev(s);
+
+   printf("Reverse of the string: %s\n", s);
+
+   return 0;
+}
+
+5. Write a function to calculate the factorial of a number (a non-negative integer).
+
+    int main()    
+{    
+ int i,fact=1,number;    
+ printf("Enter a number: ");    
+  scanf("%d",&number);    
+    for(i=1;i<=number;i++){    
+      fact=fact*i;    
+  }    
+  printf("Factorial of %d is: %d",number,fact);    
+return 0;  
+}   
+
+6. Write a function to check whether a number is in a given range.
+    
+    int main() {
+    int num, min, max;
+     
+    printf("Enter an integer\n");
+    scanf("%d", &num);
+    printf("Enter the minimum and maximum range\n");
+    scanf("%d %d", &min, &max);
+     
+    if((num-min)*(num-max) <= 0){
+        printf("%d is in range of [%d, %d]", num, min, max);
+    } else {
+     printf("%d is not in range of [%d, %d]", num, min, max);
+    }
+ 
+    return 0;
+}
+
+7. Write a function that accepts a string and calculate the number of upper case letters and lower case letters.
+    
+    int main()
+{
+    char    str[100];
+    int countL,countU;
+    int counter;
+ 
+    countL=countU=0;
+ 
+    printf("Enter a string: ");
+    gets(str);
+ 
+    for(counter=0;str[counter]!=NULL;counter++){
+ 
+        if(str[counter]>='A' && str[counter]<='Z')
+            countU++;
+        else if(str[counter]>='a' && str[counter]<='z')
+            countL++;   
+    }
+ 
+    printf("Total Upper case characters: %d, Lower Case characters: %d",countU,countL);
+ 
+    return 0;
+}
+
+8. Write a function that takes a list and returns a new list with unique elements of the first list.
+    
+int remove_duplicate(int arr[], int n)
+{
+
+  if (n == 0 || n == 1)
+    return n;
+
+  int temp[n];
+
+  int j = 0;
+  int i;
+  for (i = 0; i < n - 1; i++)
+    if (arr[i] != arr[i + 1])
+      temp[j++] = arr[i];
+  temp[j++] = arr[n - 1];
+
+  for (i = 0; i < j; i++)
+    arr[i] = temp[i];
+
+  return j;
+}
+
+int main()
+{
+  int n;
+  scanf("%d", &n);
+  int arr[n];
+  int i;
+  for (i = 0; i < n; i++)
+  {
+    scanf("%d", &arr[i]);
+  }
+  printf("\nArray Before Removing Duplicates: ");
+  for (i = 0; i < n; i++)
+    printf("%d ", arr[i]);
+
+  n = remove_duplicate(arr, n);
+
+  printf("\nArray After Removing Duplicates: ");
+  for (i = 0; i < n; i++)
+    printf("%d ", arr[i]);
+
+  return 0;
+}
+
